17eeea35c462276533aaf8efe93d6e28b24a1638
Asim Poudel <asim.pdl56@gmail.com>
2025-06-27 22:42:53 +0545

    some changes made to arrange some code and index page is added for landing url of dashboard

--- cypress/e2e/dashboard/dashboard.cy.js
+++ cypress/e2e/dashboard/dashboard.cy.js
@@ -3,7 +3,7 @@ describe('Dashboard Page', () => {
       cy.viewport(1920, 1080);
       cy.visit('https://app.acharyatech.com');
     });
-  
+    
     it('Sidebar should be visible after page load', () => {
       cy.checkSidebarVisibility();
     });
@@ -18,6 +18,18 @@ describe('Dashboard Page', () => {
   
     it('Logo should redirect to home when clicked', () => {
       cy.checkLogoRedirection();
+    });
+
+    it('Dashboard content must be visible', () => {
+        //As this is in under constructions now. so i just checked if the content or particular div is exist and visible or not!!!
+        
+        // Check main layout container is visible
+        cy.get('.minimal__layout__main').should('exist').and('be.visible');
+  
+        // Check dashboard card container is visible
+        cy.get('.MuiPaper-root.MuiPaper-elevation.MuiPaper-rounded.MuiPaper-elevation0.MuiCard-root')
+            .should('exist')
+            .and('be.visible');
     });
   });
   
---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,23 @@
+describe('Dashboard Landing page after Login', () => {
+    beforeEach(() => {
+      cy.viewport(1920, 1080);
+      cy.visit('https://app.acharyatech.com');
+    });
+  
+    it('Sidebar should be visible after page load', () => {
+      cy.checkSidebarVisibility();
+    });
+  
+    it('Sidebar should minimize and expand properly', () => {
+      cy.toggleSidebarAndCheck();
+    });
+  
+    it('Sidebar links should navigate correctly', () => {
+      cy.checkSidebarLinksNavigation();
+    });
+  
+    it('Logo should redirect to home when clicked', () => {
+      cy.checkLogoRedirection();
+    });
+  });
+  
