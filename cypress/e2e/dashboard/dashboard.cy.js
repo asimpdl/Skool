a5a961b0d3ac4529028bf6d8705c7e941c823724
Asim Poudel <asim.pdl56@gmail.com>
2025-06-27 22:10:20 +0545

    Repetition of dashboard side barr and list of links tests is added in command.js for code reusability

--- cypress/e2e/dashboard/dashboard.cy.js
+++ cypress/e2e/dashboard/dashboard.cy.js
@@ -1,75 +1,23 @@
 describe('Dashboard Page', () => {
     beforeEach(() => {
-        cy.viewport(1920,1080)
-        cy.visit('https://app.acharyatech.com');
+      cy.viewport(1920, 1080);
+      cy.visit('https://app.acharyatech.com');
     });
-    
-    it('Side bar visiblity Checking after page loads ', () => {
-        cy.get('#root__layout').should('be.visible')
-        cy.get('nav.minimal__nav__section__vertical').should('be.visible')
-        cy.get('nav ul.minimal__nav__ul  li.minimal__nav__li').should('be.visible')
+  
+    it('Sidebar should be visible after page load', () => {
+      cy.checkSidebarVisibility();
     });
-
-    it('Check Minimize and Expand Sidebar working properly', () => {
-        //Check if the toggle button is visible or not
-        cy.get('.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall').should('exist')
-        
-        // Initially check: Vertical nav is visible, mini nav is not exist
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__vertical').should('be.visible');
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__mini').should('not.exist');
-
-        // Click to Minimize sidebar
-        cy.get('.minimal__layout__nav__root .MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall')
-            .should('exist')
-            .click();
-        
-        // After minimizing: Vertical nav should not exist, mini nav should be visible
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__vertical').should('not.exist');
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__mini').should('be.visible');
-
-        // Click again to Expand sidebar
-        cy.get('.minimal__layout__nav__root .MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall')
-            .should('exist')
-            .click();
-
-        // After expanding: Vertical nav should be visible , mini nav should not exist
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__vertical').should('be.visible');
-        cy.get('.minimal__layout__nav__root nav.minimal__nav__section__mini').should('not.exist');
-
+  
+    it('Sidebar should minimize and expand properly', () => {
+      cy.toggleSidebarAndCheck();
     });
-      
-    it('Should navigate correctly when sidebar links are clicked', () => {
-        cy.get('nav ul.minimal__nav__ul li.minimal__nav__li  a')
-            .each(($link, index) => {
-                // Get the href attribute before clicking
-                const href = $link.attr('href');
-                // Re-fetch DOM elements after a click & navigation
-                cy.get('nav ul.minimal__nav__ul li.minimal__nav__li  a').eq(index).click();
-                // Now assert if URL includes the href (or exact match)
-                cy.url().should('include', href);
-                // Go back to homepage or reset state for next iteration
-                cy.visit('https://app.acharyatech.com');
-            });
+  
+    it('Sidebar links should navigate correctly', () => {
+      cy.checkSidebarLinksNavigation();
     });
-
-    it('Check logo is working or not', () => {
-
-        //Check if the logo is visible or not 
-        cy.get('.MuiBox-root .minimal__logo__root').should('exist').should('be.visible');
-
-        //Check if Logo is redirecting to baseURL or not while clicking
-        cy.get('.MuiBox-root .minimal__logo__root')
-            .should('have.attr', 'href', '/')
-            .click();
-
-        // Click to Minimize sidebar
-        cy.get('.minimal__layout__nav__root .MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall')
-            .should('exist')
-            .click();
-        
-        //Check if Logo is redirecting to baseURL or not while clicking
-        cy.get('.MuiBox-root .minimal__logo__root')
-            .should('have.attr', 'href', '/')
-            .click();
-    })
+  
+    it('Logo should redirect to home when clicked', () => {
+      cy.checkLogoRedirection();
+    });
   });
+  
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,47 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('checkSidebarVisibility', () => {
+    cy.get('#root__layout').should('be.visible');
+    cy.get('nav.minimal__nav__section__vertical').should('be.visible');
+    cy.get('nav ul.minimal__nav__ul li.minimal__nav__li').should('be.visible');
+  });
+  
+  Cypress.Commands.add('toggleSidebarAndCheck', () => {
+    const toggleBtn = '.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall';
+    const verticalNav = '.minimal__layout__nav__root nav.minimal__nav__section__vertical';
+    const miniNav = '.minimal__layout__nav__root nav.minimal__nav__section__mini';
+  
+    cy.get(toggleBtn).should('exist').and('be.visible');
+  
+    // Minimize
+    cy.get(toggleBtn).click();
+    cy.get(verticalNav).should('not.exist');
+    cy.get(miniNav).should('exist').and('be.visible');
+  
+    // Expand
+    cy.get(toggleBtn).click();
+    cy.get(verticalNav).should('exist').and('be.visible');
+    cy.get(miniNav).should('not.exist');
+  });
+  
+  Cypress.Commands.add('checkSidebarLinksNavigation', () => {
+    cy.get('nav ul.minimal__nav__ul li.minimal__nav__li a')
+      .each(($link, index) => {
+        const href = $link.attr('href');
+        cy.get('nav ul.minimal__nav__ul li.minimal__nav__li a')
+          .eq(index)
+          .click();
+        cy.url().should('include', href);
+        cy.visit('https://app.acharyatech.com'); // Reset for next loop
+      });
+  });
+  
+  Cypress.Commands.add('checkLogoRedirection', () => {
+    const logo = '.MuiBox-root .minimal__logo__root';
+    const toggleBtn = '.minimal__layout__nav__root .MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeSmall';
+  
+    cy.get(logo).should('exist').and('be.visible');
+    cy.get(logo).should('have.attr', 'href', '/').click();
+  
+    cy.get(toggleBtn).click();
+    cy.get(logo).should('have.attr', 'href', '/').click();
+  });
+  
