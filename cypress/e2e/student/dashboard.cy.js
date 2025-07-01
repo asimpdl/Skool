16708dd0813a9f6fc11888f5aab124386f92a77c
Asim Poudel <asim.pdl56@gmail.com>
2025-07-01 10:00:17 +0545

    programs test ui

--- cypress/e2e/student/dashboard.cy.js
+++ cypress/e2e/student/dashboard.cy.js
@@ -26,7 +26,7 @@
 
 // describe('Student Section: Menu Navigation and Submenu Verification', () => {
 //   beforeEach(() => {
-//     cy.visit('https://app.acharyatech.com/student');
+//     cy.visit('https://app.acharyatech.com/student')
 //   });
 
 //   it('should show and navigate all student menu items including Settings and Certificate submenu', () => {
@@ -49,131 +49,154 @@
   
 //     // Loop through main menu URLs
 //     mainUrls.forEach((url) => {
-//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click();
-//       cy.url().should('include', url);
+//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click()
+//       cy.url().should('include', url)
 //     });
   
 //     // Click the "Programs" div
 //     cy.contains('.minimal__nav__item__texts', 'Programs')
 //       .should('exist')
 //       .and('be.visible')
-//       .click();
-//     cy.url().should('include','https://app.acharyatech.com/student');
+//       .click()
+//     cy.url().should('include','https://app.acharyatech.com/student')
      
 //     // Click the "Certificate" div
 //     cy.contains('.minimal__nav__item__texts', 'Certificate')
 //       .should('exist')
 //       .and('be.visible')
-//       .click();
+//       .click()
   
 //     // Loop through Certificate sub-menu items
 //     certificateSubUrls.forEach((url) => {
-//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click();
-//       cy.url().should('include', url);
+//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click()
+//       cy.url().should('include', url)
 //     });
   
 //     // Click the "Settings" div
 //     cy.contains('.minimal__nav__item__texts', 'Settings')
 //       .should('exist')
 //       .and('be.visible')
-//       .click();
+//       .click()
   
 //     // Loop through Setting sub-menu items
 //     settingSubUrls.forEach((url) => {
-//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click();
+//       cy.get(`a[href="${url}"]`).should('exist').and('be.visible').click()
 //       cy.url().should('include', url);
+//     })
+//   });
+// });
+
+// describe('Dashboard: Main Layout Structure and Behavior', () => {
+//     beforeEach(() => {
+//       cy.visit('https://app.acharyatech.com')
+//       // Navigate to dashboard
+//       cy.get('a[href="/student"]').click()
+//       cy.get('a[href="/student/dashboard"]').click()
 //     });
-//   });
-// });
 
-// describe('Main Layout Structure and Behavior', () => {
-//   beforeEach(() => {
-//     cy.visit('https://app.acharyatech.com/student');
-//   });
+//   it('Dashboard Count Data Display', () => {
+      
+//     // Confirm the summary cards container is visible
+//     cy.get('.MuiCardContent-root').should('be.visible')
+//     cy.get('.MuiPaper-elevation > .MuiBox-root').should('be.visible')
+  
+//     // Labels to check
+//     const labels = [
+//       'Current Students',
+//       'Pending Admission',
+//       'Total Parents',
+//       'Total Certificates',
+//     ];
+  
+//     // Check each label and its numeric value
+//     labels.forEach((label) => {
+//       cy.contains(label)
+//         .should('be.visible')
+//         .parent() // go to wrapper div
+//         .should('be.visible')
+//         .within(() => {
+//           cy.get('div').first().invoke('text').should('match', /^\d+$/)   // number exists
+//         });
+//     });
+//   });  
 
-//   it('Dashboard  div Display', () => {
-//     cy.get('a[href="/student"]').should('exist').and('be.visible')
-//       .click();
-//     cy.get('a[href="/student/dashboard"]').should('exist').and('be.visible')
-//       .click();
+//   // it('Dashboard Count Data Should Match API', () => {
+
+//   //   // Navigate to dashboard
+//   //   cy.get('a[href="/student"]').click()
+//   //   cy.get('a[href="/student/dashboard"]').click()
+//   //   cy.wait(100)
+//   //   cy.request('https://m2.acharyatech.com/student/dashboard/status').then((response) => {
+//   //     cy.log(JSON.stringify(response.body))
+//   //     const apiData = response.body
+  
+//   //     const labelToKeyMap = {
+//   //       'Current Students': apiData.students,
+//   //       'Pending Admission': apiData.pendingAdmission,
+//   //       'Total Parents': apiData.parents,
+//   //       'Total Certificates': apiData.certificates,
+//   //     };
+      
+//   //     // Loop over each label and compare number from UI with API
+//   //     Object.entries(labelToKeyMap).forEach(([label, expectedValue]) => {
+//   //         cy.contains(label)
+//   //           .should('be.visible')
+//   //           .parent()
+//   //         .within(() => {
+//   //           cy.get('div')
+//   //             .first()
+//   //             .invoke('text')
+//   //             .then((text) => {
+//   //                 const uiValue = parseInt(text.trim(), 10)
+//   //                 expect(uiValue).to.eq(expectedValue)
+//   //               });
+//   //         });
+//   //     });      
+//   //   }); 
+//   // });
+  
+//   it('Student Dashboard: Charts and Table', () => {
     
-//     cy.get('.MuiBreadcrumbs-ol').should('exist').and('be.visible');
-
-//     cy.get('.MuiCardHeader-content > .MuiTypography-root').should('exist').and('be.visible');
-
-//     cy.get('.MuiCardContent-root').should('exist').and('be.visible');
-
-//   });
-
+//     // cy.get('.MuiCard-root .MuiCardHeader-root .MuiCardHeader-content .MuiCardHeader-title')
+//     //   .should('exist').and('be.visible')
+//     cy.get('.MuiGrid2-root').should('exist').and('be.visible')
+//   }); 
 // });
 
-describe('Main Layout Structure and Behavior', () => {
+// describe('Programs', () => {
+//   beforeEach(() => {
+//     cy.visit('https://app.acharyatech.com')
+//     // Navigate to program
+//     cy.get('a[href="/student"]').click()
+//     // cy.get('a[href="/student/program"]').click()
+//   });
+
+//   it('Programs visibility', () => {
+//     cy.get('.MuiBox-root .MuiGrid2-root .MuiCard-root .MuiStack-root').should('exist').and('be.visible')
+//     cy.get('svg[role="img"]').should('exist').and('be.visible');
+//     cy.get('p').should('exist').and('be.visible');
+//   });
+
+//   // it('Programs visibility and redirection', () => {
+//   //   // Make sure all program cards are visible
+//   //   cy.get('.minimal__layout__main .MuiBox-root .MuiGrid2-root .MuiCard-root').should('exist').and('be.visible');
+//   //   //
+//   // });
+// });
+
+describe('Admission', () => {
   beforeEach(() => {
-    cy.visit('https://app.acharyatech.com');
+    cy.visit('https://app.acharyatech.com')
+    // Navigate to program
+    cy.get('a[href="/student"]').click()
+    cy.get('a[href="/student/admission"]').click()
   });
 
-  it('Dashboard Count Data Display', () => {
-    // Navigate to dashboard
-    cy.get('a[href="/student"]').click();
-    cy.get('a[href="/student/dashboard"]').click();
-  
-    // Confirm the summary cards container is visible
-    cy.get('.MuiCardContent-root').should('be.visible');
-    cy.get('.MuiPaper-elevation > .MuiBox-root').should('be.visible');
-  
-    // Labels to check
-    const labels = [
-      'Current Students',
-      'Pending Admission',
-      'Total Parents',
-      'Total Certificates',
-    ];
-  
-    // Check each label and its numeric value
-    labels.forEach((label) => {
-      cy.contains(label)
-        .should('be.visible')
-        .parent() // go to wrapper div
-        .should('be.visible')
-        .within(() => {
-          cy.get('div').first().invoke('text').should('match', /^\d+$/); // number exists
-        });
-    });
-  });  
-
-  it('Dashboard Count Data Should Match API', () => {
-
-    // Navigate to dashboard
-    cy.get('a[href="/student"]').click();
-    cy.get('a[href="/student/dashboard"]').click();
-    cy.wait(100);    
-    cy.request('https://m2.acharyatech.com/student/dashboard/status').then((response) => {
-      cy.log(JSON.stringify(response.body));
-      const apiData = response.body;
-  
-      const labelToKeyMap = {
-        'Current Students': apiData.students,
-        'Pending Admission': apiData.pendingAdmission,
-        'Total Parents': apiData.parents,
-        'Total Certificates': apiData.certificates,
-      };
-      
-      // Loop over each label and compare number from UI with API
-      Object.entries(labelToKeyMap).forEach(([label, expectedValue]) => {
-          cy.contains(label)
-            .should('be.visible')
-            .parent()
-          .within(() => {
-            cy.get('div')
-              .first()
-              .invoke('text')
-              .then((text) => {
-                  const uiValue = parseInt(text.trim(), 10);
-                  expect(uiValue).to.eq(expectedValue);
-                });
-          });
-      });      
-    }); 
-  });  
+  // it('Admission UI Visibility', () => {
+  //   cy.get('.MuiBox-root .MuiGrid2-root .MuiCard-root .MuiStack-root').should('exist').and('be.visible')
+  //   cy.get('svg[role="img"]').should('exist').and('be.visible');
+  //   cy.get('p').should('exist').and('be.visible');
+  // });
 });
+  
 
